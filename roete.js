43810eacb8884c3775c91d4c91d0ede3537a95f2
Arnout Kazemier <info@3rd-Eden.com>
2015-01-21 12:06:50 +0100

    [major] Complete the route parser.

---
+++ roete.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var dollars = require('dollars');
+
+/**
+ * Roete (Route) matching.
+ *
+ * @constructor
+ * @param {String} url URL we should trigger upon.
+ * @api public
+ */
+function Roete(url) {
+  if (!this) return new Roete(url);
+
+  this.parsers = {};    // Parameter parsers.
+  this.params = [];     // Name of the parsers.
+  this.path = null;     // The compiled Regular Expression.
+
+  this.parse(url);
+}
+
+/**
+ * Add a new parameter parser.
+ *
+ * @param {String} name Name of the param that we should parse.
+ * @param {Function} fn Parser method.
+ * @returns {Roete}
+ * @api public
+ */
+Roete.prototype.param = function param(name, fn) {
+  this.parsers[name] = fn;
+  return this;
+};
+
+/**
+ * Transform a given URL to a regular expression so we can match against
+ * incoming URL's.
+ *
+ * @param {String} url The URL or Regular-like-Expression we should match.
+ * @returns {Roete}
+ * @api public
+ */
+Roete.prototype.parse = function parse(url) {
+  if (url.charAt(url.length - 1) === '/') url = url.slice(0, -1);
+  if (url.charAt(0) === '/') url = url.slice(1);
+  url = url.split('/');
+
+  var roete = this
+    , slash = '\\/';
+
+  roete.path = new RegExp('^'+ slash + dollars.array.map(url, function each(frag) {
+    return frag.replace(/\{([^\{]+?)\}/g, function replace(match, key) {
+      roete.params.push(key);
+
+      return '([a-zA-Z0-9-_~\\.%]+)';
+    });
+  }).join(slash) + slash);
+
+  return roete;
+};
+
+/**
+ * Check if the route is matching the given URL.
+ *
+ * @param {String} url Pathname we need to match.
+ * @returns {String|Undefined} Matches URL.
+ * @api public
+ */
+Roete.prototype.match = function matching(url) {
+  if (url.charAt(url.length - 1) !== '/') url = url +'/';
+  if (url.charAt(0) !== '/') url = '/'+ url;
+
+  var slices = this.path.exec(url)
+    , roete = this
+    , match;
+
+  if (slices) match = new Match(slices, url);
+  if (!slices || slices.length === 1) return match;
+
+  return slices.slice(1).reduce(function reduce(memo, arg, index) {
+    var param = roete.params[index];
+
+    match.params[param] = roete.parsers[param]
+    ? roete.parsers[param](arg)
+    : arg;
+
+    return match;
+  }, match);
+};
+
+/**
+ * Representation of a matched route.
+ *
+ * @constructor
+ * @param {Array} slices The matches from the Roete RegExp.
+ * @param {String} url The URL that we matched.
+ * @api private
+ */
+function Match(slices, url) {
+  this.url = url.slice(slices[0].length);
+  this.match = slices[0];
+  this.matches = slices;
+  this.params = {};
+}
+
+//
+// Expose the interface.
+//
+Roete.Match = Match;
+module.exports = Roete;
--- test.js
+++ test.js
@@ -1,14 +1,71 @@
-describe('magicarp', function () {
+describe('Roete', function () {
   'use strict';
 
-  var Magicarp = require('./')
+  var Roete = require('./roete')
     , assume = require('assume');
 
   it('is exported as function', function () {
-    assume(Magicarp).is.a('function');
+    assume(Roete).is.a('function');
   });
 
-  it('exposes the .Fragment constructor', function () {
-    assume(Magicarp.Fragment).is.a('function');
+  it('can be initialized without `new` keyword', function () {
+    var route = Roete('foo')
+      , res = route.match('foo');
+
+    assume(res).is.instanceOf(Roete.Match);
+  });
+
+  it('contains the rest of URL', function () {
+    var route = new Roete('foo')
+      , res = route.match('foo/bar');
+
+    assume(res.url).contains('bar');
+    assume(res.url).does.not.contains('foo');
+    assume(res.match).equals('/foo/');
+  });
+
+  [
+    { url: '/foo', matches: '/foo/' },
+    { url: '/{hello}/{world}/', matches: '/hello/mom/' },
+    { url: '{hello}', matches: '/things/' },
+    { url: 'hello', matches: '/hello/' },
+    { url: 'hello', matches: 'hello' },
+    { url: 'hello', matches: '/hello/world/' },
+    { url: 'hello', matches: 'hello/world' }
+  ].forEach(function each(spec) {
+    it('matches '+ spec.url +' with '+ spec.matches, function () {
+      var route = new Roete(spec.url)
+        , res = route.match(spec.matches);
+
+      assume(res).is.not.undefined();
+    });
+  });
+
+  it('returns undefined when the route is not matched', function () {
+    var route = new Roete('/hello/{world}/')
+      , match = route.match('/hola/foo');
+
+    assume(match).is.undefined();
+  });
+
+  it('defaults to the value in the path', function () {
+    var route = new Roete('/hello/{world}/')
+      , match = route.match('/hello/foo');
+
+    assume(match.params.world).equals('foo');
+  });
+
+  describe('.param', function () {
+    it('adds parsers that transform values', function () {
+      var route = new Roete('/hello/{world}/');
+
+      route.param('world', function (arg) {
+        assume(arg).equals('foo');
+        return 'bar';
+      });
+
+      var match = route.match('/hello/foo');
+      assume(match.params.world).equals('bar');
+    });
   });
 });
